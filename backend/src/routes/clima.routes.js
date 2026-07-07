2176f4890e8a87e1c830331ba50f59501ecfdbf8
Zuniga Carlos Gadiel <138547044+u22222779@users.noreply.github.com>
2026-07-06 22:38:21 -0500

    Delete backend directory

--- backend/src/routes/clima.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const climaController = require("../controllers/clima.controller");
-
-router.post("/guardar", climaController.guardarClima);
-
-module.exports = router;
